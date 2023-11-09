ca6fe5f56b9531fc8327d955a6f2523a975b8da8
Max <max@maximilian.co.nz>
2023-11-09 23:57:06 +1300

    Setup basic plugin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# teleport-utils
-Minecraft plugin that adds /spawn, /home, and /tpa
+# Teleport Utils
+![Minecraft Server Version](https://img.shields.io/badge/Minecraft%20Version-1.20.1-yellow?style=for-the-badge)
+
+---
+
+Minecraft plugin that adds `/spawn`, `/home`, and `/tpa` commands to your server.
---
+++ src/net/maxtheminerboy/Main.java
@@ -0,0 +1,20 @@
+package net.maxtheminerboy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+		getLogger().info("Teleport Utils enabled!");
+	}
+
+	
+
+	@Override
+	public void onDisable() {
+		
+		getLogger().info("Teleport Utils enabled!");
+	}
+}
