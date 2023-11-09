47ec721d6303626518c5bf5646d66af8e76cc040
Max <max@maximilian.co.nz>
2023-11-10 02:37:14 +1300

    Added `/spawn` and `/setSpawn` commands

--- src/net/maxtheminerboy/Main.java
+++ src/net/maxtheminerboy/Main.java
@@ -2,11 +2,35 @@ package net.maxtheminerboy;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.maxtheminerboy.commands.spawn.SetSpawnCommand;
+import net.maxtheminerboy.commands.spawn.SpawnCommand;
+
 public class Main extends JavaPlugin {
+
+	private static Main instance;
 
 	@Override
 	public void onEnable() {
 		
+		// Check for if there is already the needed config files. If not
+		// then create them and fill with default information
+		instance = this;
+		Config.createConfigs();
+		
+
+		// Register all the spawn related commands
+		getCommand("setspawn").setExecutor(new SetSpawnCommand());
+		getCommand("spawn").setExecutor(new SpawnCommand());
+
+		// Register all of the home related commands
+
+
+		// Register all of the tpa related command
+
+
+
+
+		// Log fancy message
 		getLogger().info("Teleport Utils enabled!");
 	}
 
@@ -15,6 +39,13 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		
-		getLogger().info("Teleport Utils enabled!");
+		// Log fancy message
+		getLogger().info("Teleport Utils disabled!");
+	}
+
+
+	public static Main getInstance()
+	{
+		return instance;
 	}
 }
---
+++ src/net/maxtheminerboy/commands/spawn/SpawnCommand.java
@@ -0,0 +1,15 @@
+package net.maxtheminerboy.commands.spawn;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SpawnCommand implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		
+		return false;
+	}
+}
